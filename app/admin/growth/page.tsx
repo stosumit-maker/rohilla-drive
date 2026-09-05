8558cea4a8245d44e758dd699fd9d0847a02aff4
stosumit-maker <stosumit@gmail.com>
2026-09-05 16:10:47 +0530

    Add Admin Rohilla Intelligence growth console

---
+++ app/admin/growth/page.tsx
@@ -0,0 +1,2 @@
+import GrowthConsole from "../../components/GrowthConsole";
+export default function AdminGrowth(){return <GrowthConsole actorRole="admin" title="Founder Growth & Automation Console" homeHref="/admin"/>}
