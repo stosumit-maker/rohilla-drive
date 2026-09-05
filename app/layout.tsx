15b7300bcc9bf8d0a3b65a3ec14d161d72fc1c0e
stosumit-maker <stosumit@gmail.com>
2026-09-05 17:16:32 +0530

    Add global hreflang language alternates

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import PublicQuickLinks from "./PublicQuickLinks";
 import LanguageExperience from "./components/LanguageExperience";
 
 const site = "https://www.rohilladrive.com";
+const languages={"en-IN":"/en","hi-IN":"/hi","pa-IN":"/pa","kn-IN":"/kn","ta-IN":"/ta","te-IN":"/te","ml-IN":"/ml","mr-IN":"/mr","gu-IN":"/gu","bn-IN":"/bn","or-IN":"/or","ur-IN":"/ur","x-default":"/"};
 
 export const metadata: Metadata = {
   metadataBase: new URL(site),
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
   description:
     "Buy, sell, verify, maintain, protect and manage vehicles through ROHILLA DRIVE in Ambala City and the growing Rohilla automotive network.",
   applicationName: "ROHILLA DRIVE",
-  alternates: { canonical: "/" },
+  alternates: { canonical: "/", languages },
   manifest: "/manifest.webmanifest",
   openGraph: {
     type: "website",
