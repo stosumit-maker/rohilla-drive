d3d14bc8ce75106d3ba406b972143ab7e472dcd1
stosumit-maker <stosumit@gmail.com>
2026-09-05 16:10:56 +0530

    Add Business Hub partner growth console

---
+++ app/partner/growth/page.tsx
@@ -0,0 +1,2 @@
+import GrowthConsole from "../../components/GrowthConsole";
+export default function PartnerGrowth(){return <GrowthConsole actorRole="partner" title="Business Hub Growth & Automation" homeHref="/partner"/>}
