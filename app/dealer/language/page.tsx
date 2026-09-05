c08c4eab8bc4db87961f45322399fbe3940450df
stosumit-maker <stosumit@gmail.com>
2026-09-05 16:43:04 +0530

    Add dealer multilingual communication desk

---
+++ app/dealer/language/page.tsx
@@ -0,0 +1,2 @@
+import TranslatorDesk from "../../components/TranslatorDesk";
+export default function DealerLanguage(){return <TranslatorDesk title="Dealer Multilingual Communication" defaultOperator="hi-IN"/>}
