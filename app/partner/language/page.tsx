4dff087e31e14a1c910b0587efc282cf774e7773
stosumit-maker <stosumit@gmail.com>
2026-09-09 15:59:00 +0530

    Rename partner language desk to Communication Centre

--- app/partner/language/page.tsx
+++ app/partner/language/page.tsx
@@ -1,2 +1,2 @@
 import TranslatorDesk from "../../components/TranslatorDesk";
-export default function PartnerLanguage(){return <TranslatorDesk title="Business Hub Multilingual Communication" defaultOperator="hi-IN" actorRole="partner"/>}
+export default function PartnerLanguage(){return <TranslatorDesk title="Partner Communication Centre" defaultOperator="hi-IN" actorRole="partner"/>}
